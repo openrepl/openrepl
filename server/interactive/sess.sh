5cbee58b21274607acb88f47fa5e305e4623972a
Jaden Weiss <jadr2ddude@gmail.com>
2017-09-09 05:48:09 -0400

    try different address

--- server/interactive/sess.sh
+++ server/interactive/sess.sh
@@ -13,8 +13,8 @@ set -x
 
 if [ $# -eq 2 ]; then
     curl http://60s/get?id="$2" > $NAME || exit 1
-    DOCKERFLAGS=(-v "$NAME:/script")
-    SARGS=(/script)
+    DOCKERFLAGS=(-v "$NAME:/tmp/script")
+    SARGS=(/tmp/script)
 fi
 
 bash filter.sh "$1" || exit 1
